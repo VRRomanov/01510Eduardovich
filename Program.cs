e2d1dfb7ddda9e8517de523f025651ecb1016574
VRRomanov <sasanikolai@yandex.ru>
2023-09-20 16:03:12 +0300

    add SystemException

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        build: SystemException = new SystemException
 
 
